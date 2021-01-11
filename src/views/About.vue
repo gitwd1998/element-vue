a86e61b4453ded833feabd123f02290a954737d7
gitwd <1420262588@qq.com>
2021-01-11 22:19:50 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,183 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-button @click="dialogVisible = true">对话框</el-button>
+    <el-tooltip
+      content="文字提示内容文字提示内容文字提示内容"
+      placement="top-start"
+    >
+      <i class="el-icon-question"></i>
+    </el-tooltip>
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false" size="mini">取消</el-button>
+        <el-button type="primary" @click="dialogVisible = false" size="mini"
+          >确定</el-button
+        >
+      </span>
+    </el-dialog>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+      <el-table-column align="right"
+        ><template slot="header">
+          <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
+        </template>
+        <template slot-scope="scope">
+          <el-button size="mini" type="danger" @click="handleDelete(scope)"
+            >Delete</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-table :data="tableChildData" row-key="id">
+      <el-table-column prop="date" label="日期" sortable width="180" />
+      <el-table-column prop="name" label="姓名" sortable width="180" />
+      <el-table-column prop="address" label="地址" />
+    </el-table>
+    <el-button @click="handleOpen">打开Message Box弹窗</el-button>
   </div>
 </template>
+<script>
+import {
+  Button,
+  Dialog,
+  Input,
+  Table,
+  TableColumn,
+  Tooltip,
+  Message,
+  MessageBox,
+} from "element-ui";
+export default {
+  components: {
+    elButton: Button,
+    elTooltip: Tooltip,
+    elDialog: Dialog,
+    elTable: Table,
+    elTableColumn: TableColumn,
+    elInput: Input,
+  },
+  data() {
+    return {
+      dialogVisible: false,
+      search: "",
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+      tableChildData: [
+        {
+          id: 1,
+          date: "2016-05-02",
+          name: "赵",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: 2,
+          date: "2016-05-04",
+          name: "钱",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          id: 3,
+          date: "2016-05-01",
+          name: "孙",
+          address: "上海市普陀区金沙江路 1519 弄",
+          children: [
+            {
+              id: 31,
+              date: "2016-05-01",
+              name: "李",
+              address: "上海市普陀区金沙江路 1519 弄",
+            },
+            {
+              id: 32,
+              date: "2016-05-01",
+              name: "周",
+              address: "上海市普陀区金沙江路 1519 弄",
+            },
+          ],
+        },
+        {
+          id: 4,
+          date: "2016-05-03",
+          name: "武",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+      tableData1: [
+        {
+          id: 1,
+          date: "2016-05-02",
+          name: "郑",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          id: 2,
+          date: "2016-05-04",
+          name: "王",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          id: 3,
+          date: "2016-05-01",
+          name: "董",
+          address: "上海市普陀区金沙江路 1519 弄",
+          hasChildren: true,
+        },
+        {
+          id: 4,
+          date: "2016-05-03",
+          name: "张",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClose(done) {
+      alert("关闭前你想干的事");
+      done();
+    },
+    handleDelete(scope) {
+      console.log(scope);
+    },
+    handleOpen() {
+      MessageBox.alert("这是一段内容", "标题名称", {
+        confirmButtonText: "确定",
+        callback: (action) => {
+          Message({
+            type: "info",
+            message: `action: ${action}`,
+          });
+        },
+      });
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,134 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button plain>朴素按钮</el-button>
+      <el-button type="primary" plain>主要按钮</el-button>
+      <el-button type="success" plain>成功按钮</el-button>
+      <el-button type="info" plain>信息按钮</el-button>
+      <el-button type="warning" plain>警告按钮</el-button>
+      <el-button type="danger" plain>危险按钮</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button round>圆角按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
+
+    <el-row>
+      <el-radio v-model="radio" label="1">备选项</el-radio>
+      <el-radio v-model="radio" label="2">备选项</el-radio>
+    </el-row>
+
+    <el-row>
+      <el-select
+        v-model="selectValue"
+        @change="handleSelect"
+        placeholder="请选择一个"
+      >
+        <el-option
+          v-for="(item, index) in options"
+          :key="index"
+          :label="item.label"
+          :value="item.id"
+        />
+      </el-select>
+      <el-select
+        v-model="multipleValue"
+        multiple
+        @change="handleSelect"
+        placeholder="全选择一个或多个"
+      >
+        <el-option
+          v-for="(item, index) in options"
+          :key="index"
+          :label="item.label"
+          :value="item.id"
+        />
+      </el-select>
+      <el-select
+        v-model="collapseValue"
+        multiple
+        collapse-tags
+        @change="handleSelect"
+        placeholder="全选择一个或多个"
+      >
+        <el-option
+          v-for="(item, index) in options"
+          :key="index"
+          :label="item.label"
+          :value="item.id"
+        />
+      </el-select>
+    </el-row>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { Button, Row, Radio, Select, Option } from "element-ui";
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    elButton: Button,
+    elRow: Row,
+    elRadio: Radio,
+    elSelect: Select,
+    elOption: Option,
+  },
+  data() {
+    return {
+      radio: "1",
+      options,
+      selectValue: "",
+      multipleValue: "",
+      collapseValue: "",
+    };
+  },
+  methods: {
+    handleSelect(value) {
+      console.log(value);
+    },
+  },
+};
+var options = [
+  {
+    id: 1,
+    label: "黄金糕",
+  },
+  {
+    id: 2,
+    label: "双皮奶",
+  },
+  {
+    id: 3,
+    label: "蚵仔煎",
+  },
+  {
+    id: 4,
+    label: "龙须面",
+  },
+  {
+    id: 5,
+    label: "北京烤鸭",
+  },
+];
 </script>
