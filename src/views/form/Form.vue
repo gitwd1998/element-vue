dd62779752147d9171c18225c4a28ed1e03dc20c
gitwd1998 <wd15855385074@outlook.com>
2021-06-07 19:49:05 +0800

    OCT 优化表单组件

--- src/views/form/Form.vue
+++ src/views/form/Form.vue
@@ -2,68 +2,199 @@
   <el-form
     :model="formData"
     :rules="rules"
-    ref="ruleForm"
-    label-width="100px"
+    ref="form"
+    :label-position="labelPosition"
+    label-width="120px"
     status-icon
   >
-    <el-form-item label="文本框" prop="input">
-      <el-input v-model="formData.input" />
+    <el-form-item label="标题对齐方式">
+      <el-radio-group v-model="labelPosition">
+        <el-radio-button label="left">左对齐</el-radio-button>
+        <el-radio-button label="right">右对齐</el-radio-button>
+        <el-radio-button label="top">顶部对齐</el-radio-button>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="输入框" prop="input">
+      <el-input v-model.trim="formData.input" clearable placeholder="请输入" />
     </el-form-item>
     <el-form-item label="下拉菜单" prop="select">
-      <el-select v-model="formData.select" style="width: 100%">
-        <el-option label="区域一" value="1" />
-        <el-option label="区域二" value="2" />
+      <el-select
+        v-model="formData.select"
+        style="width: 100%"
+        clearable
+        placeholder="请选择"
+      >
+        <el-option label="选项一" :value="1" />
+        <el-option label="选项二" :value="2" />
+        <el-option label="选项三" :value="3" />
       </el-select>
     </el-form-item>
-    <el-form-item label="日期选择器" required>
+    <el-form-item label="日期时间" required>
       <el-col :span="11">
-        <el-form-item prop="data">
+        <el-form-item prop="date">
           <el-date-picker
+            clearable
             :editable="false"
             type="date"
-            v-model="formData.data"
+            v-model="formData.date"
+            format="yyyy-MM-dd"
+            value-format="yyyy-MM-dd"
             style="width: 100%"
-            format="yyyy年mm月dd日"
-            value-format="yyyy-mm-dd"
+            placeholder="请选择日期"
           />
         </el-form-item>
       </el-col>
-      <el-col align="center" :span="2">-</el-col>
+      <el-col align="center" :span="2"> # </el-col>
       <el-col :span="11">
         <el-form-item prop="time">
           <el-time-picker
+            clearable
             :editable="false"
             v-model="formData.time"
-            style="width: 100%"
-            format="hh时mm分ss秒"
+            format="hh:mm:ss"
             value-format="hh:mm:ss"
+            style="width: 100%"
+            placeholder="请选择时间"
           />
         </el-form-item>
       </el-col>
+    </el-form-item>
+    <el-form-item label="日期范围" required>
+      <el-col :span="11">
+        <el-form-item prop="start">
+          <el-date-picker
+            clearable
+            :editable="false"
+            type="date"
+            v-model="formData.start"
+            style="width: 100%"
+            format="yyyy-MM-dd"
+            placeholder="请选择开始日期"
+            :picker-options="startPickerOption"
+          />
+        </el-form-item>
+      </el-col>
+      <el-col align="center" :span="2"> - </el-col>
+      <el-col :span="11">
+        <el-form-item prop="end">
+          <el-date-picker
+            clearable
+            :editable="false"
+            type="date"
+            v-model="formData.end"
+            style="width: 100%"
+            placeholder="请选择结束日期"
+            :picker-options="endPickerOption"
+          />
+        </el-form-item>
+      </el-col>
+    </el-form-item>
+    <el-form-item label="日期范围" prop="dateRange">
+      <el-date-picker
+        style="width: 100%"
+        v-model="formData.dateRange"
+        type="daterange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+      >
+      </el-date-picker>
+    </el-form-item>
+    <el-form-item label="时间范围" prop="timeRange">
+      <el-time-picker
+        style="width: 100%"
+        is-range
+        arrow-control
+        v-model="formData.timeRange"
+        range-separator="至"
+        start-placeholder="开始时间"
+        end-placeholder="结束时间"
+        placeholder="选择时间范围"
+      >
+      </el-time-picker>
     </el-form-item>
     <el-form-item label="开关" prop="switch">
       <el-switch v-model="formData.switch" />
     </el-form-item>
     <el-form-item label="多选框" prop="checkbox">
-      <el-checkbox-group v-model="formData.checkbox">
-        <el-checkbox label="A" />
-        <el-checkbox label="B" />
-        <el-checkbox label="C" />
-        <el-checkbox label="D" />
+      <el-checkbox
+        :indeterminate="isIndeterminate"
+        v-model="checkAll"
+        @change="handleCheckAll"
+        >全选</el-checkbox
+      >
+      <el-checkbox-group v-model="formData.checkbox" @change="handleCheck">
+        <el-checkbox :label="1">选项一</el-checkbox>
+        <el-checkbox :label="2">选项二</el-checkbox>
+        <el-checkbox :label="3">选项三</el-checkbox>
       </el-checkbox-group>
     </el-form-item>
     <el-form-item label="单选框" prop="radio">
       <el-radio-group v-model="formData.radio">
-        <el-radio label="A" />
-        <el-radio label="B" />
+        <el-radio :label="1">选项一</el-radio>
+        <el-radio :label="2">选项二</el-radio>
+        <el-radio :label="3">选项三</el-radio>
       </el-radio-group>
     </el-form-item>
     <el-form-item label="文本框" prop="textarea">
-      <el-input type="textarea" v-model="formData.textarea" />
+      <el-input type="textarea" v-model.trim="formData.textarea" />
+    </el-form-item>
+    <el-form-item label="表格" required>
+      <el-table :data="formData.table">
+        <el-table-column label="序号" type="index" align="center" :width="80" />
+        <el-table-column label="姓名" prop="name">
+          <template slot-scope="scope">
+            <el-form-item
+              :prop="'table.' + scope.$index + '.' + 'name'"
+              :rules="rules.name"
+            >
+              <el-input v-model="scope.row.name" />
+            </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column label="性别" prop="sex">
+          <template slot-scope="scope">
+            <el-form-item
+              :prop="'table.' + scope.$index + '.' + 'sex'"
+              :rules="rules.sex"
+            >
+              <el-input v-model="scope.row.sex" />
+            </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column label="年龄" prop="age">
+          <template slot-scope="scope">
+            <el-form-item
+              :prop="'table.' + scope.$index + '.' + 'age'"
+              :rules="rules.age"
+            >
+              <el-input-number v-model="scope.row.age" :min="1" />
+            </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" align="center">
+          <template slot-scope="{ row }">
+            <el-button @click="handleDelete(row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-form-item>
+    <el-form-item label="上传附件" prop="fileList">
+      <el-upload
+        action
+        :on-change="handleChangeFile"
+        :file-list="formData.fileList"
+        list-type="text"
+        :auto-upload="false"
+        show-file-list
+        multiple
+      >
+        <el-button slot="trigger">选取文件</el-button>
+      </el-upload>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="handleSubmit">提交</el-button>
-      <el-button @click="handleReset">重置</el-button>
+      <el-button type="success" @click="handleSubmit">提交</el-button>
+      <el-button type="danger" @click="handleReset">重置</el-button>
     </el-form-item>
   </el-form>
 </template>
@@ -83,71 +214,143 @@ import {
   Select,
   DatePicker,
   Switch,
+  RadioButton,
+  Table,
+  TableColumn,
+  Upload,
+  Message,
+  InputNumber,
 } from "element-ui";
+import { upload } from "@/api";
 export default {
   components: {
     elForm: Form,
     elFormItem: FormItem,
     elInput: Input,
+    elInputNumber: InputNumber,
     elSelect: Select,
     elOption: Option,
     elCheckboxGroup: CheckboxGroup,
     elCheckbox: Checkbox,
     elRadioGroup: RadioGroup,
     elRadio: Radio,
+    elRadioButton: RadioButton,
     elTimePicker: TimePicker,
     elDatePicker: DatePicker,
     elSwitch: Switch,
     elButton: Button,
     elCol: Col,
+    elTable: Table,
+    elTableColumn: TableColumn,
+    elUpload: Upload,
   },
   data() {
     return {
+      labelPosition: "right",
+      isIndeterminate: true,
+      checkAll: false,
+      startPickerOption: {
+        disabledDate: (d) => {
+          if (this.formData.end)
+            return !(
+              d.getTime() > Date.now() - 8.64e7 &&
+              d.getTime() < new Date(this.formData.end).getTime() + 8.64e7
+            );
+          return d.getTime() < new Date() - 8.64e7;
+        },
+      },
+      endPickerOption: {
+        disabledDate: (d) => {
+          if (this.formData.start) return d.getTime() < this.formData.start;
+          return d.getTime() < Date.now() - 8.64e7;
+        },
+      },
       formData: {
         input: "",
-        select: "",
-        data: "",
+        select: 2,
+        date: "",
         time: "",
+        start: "",
+        end: "",
+        dateRange: [new Date(2020, 9, 10, 8, 40), new Date(2020, 9, 10, 9, 40)],
+        timeRange: [new Date(2020, 9, 10, 8, 40), new Date(2020, 9, 10, 9, 40)],
         switch: false,
-        checkbox: [],
-        radio: "",
+        checkbox: [1],
+        radio: 1,
         textarea: "",
+        table: [
+          { name: "", sex: "", age: 10 },
+          { name: "", sex: "", age: 10 },
+        ],
+        fileList: [],
+        upload: null,
       },
       rules: {
         input: [
-          { required: true, message: "活动名称为必填字段", trigger: "blur" },
+          { required: true, message: "请输入", trigger: "blur" },
           { min: 3, max: 5, message: "长度在3 ~ 5个字符", trigger: "blur" },
         ],
-        select: [
-          { required: true, message: "活动区域为必选项", trigger: "change" },
-        ],
-        data: [{ required: true, message: "请选择日期", trigger: "change" }],
+        select: [{ required: true, message: "请选择", trigger: "change" }],
+        date: [{ required: true, message: "请选择日期", trigger: "change" }],
         time: [{ required: true, message: "请选择时间", trigger: "change" }],
-        checkbox: [
-          { required: true, message: "至少选择一项", trigger: "change" },
+        start: [{ required: true, message: "请选择日期", trigger: "change" }],
+        end: [{ required: true, message: "请选择日期", trigger: "change" }],
+        dateRange: [
+          { required: true, message: "请选择日期范围", trigger: "change" },
         ],
-        radio: [
-          { required: true, message: "请选择活动资源", trigger: "change" },
+        timeRange: [
+          { required: true, message: "请选择时间范围", trigger: "change" },
         ],
-        textarea: [
-          { required: true, message: "请填写活动形式", trigger: "blur" },
+        checkbox: [{ required: true, message: "请选择", trigger: "change" }],
+        radio: [{ required: true, message: "请选择", trigger: "change" }],
+        textarea: [{ required: true, message: "请输入", trigger: "blur" }],
+        upload: [{ required: true, message: "请输入", trigger: "blur" }],
+        fileList: [
+          { required: true, message: "请选择文件", trigger: "change" },
         ],
+        name: [{ required: true, message: "请输入性别", trigger: "blur" }],
+        sex: [{ required: true, message: "请输入性别", trigger: "blur" }],
+        age: [{ required: true, message: "请输入年龄", trigger: "blur" }],
       },
     };
   },
   methods: {
+    handleCheckAll(val) {
+      this.formData.checkbox = val ? [1, 2, 3] : [];
+      this.isIndeterminate = false;
+    },
+    handleCheck(val) {
+      let len = val.length;
+      this.checkAll = len === 3;
+      this.isIndeterminate = len > 0 && len < 3;
+    },
+    handleDelete(row) {
+      console.log(row);
+    },
+    handleChangeFile(file, fileList) {
+      this.formData.fileList = fileList;
+    },
     handleSubmit() {
-      this.$refs.ruleForm.validate((valid, rules) => {
+      this.$refs.form.validate(async (valid, rules) => {
         if (valid) {
+          this.formData.upload = new FormData();
+          this.formData.fileList.forEach((v) => {
+            this.formData.upload.append(v.name, v.raw);
+          });
+          let { data } = await upload(this.formData.upload);
+          if (Number(data.code) === 0) {
+            this.formData.fileList = data.data;
+          } else {
+            Message.error(data.msg);
+          }
           console.log(this.formData);
         } else {
-          console.log(valid, rules);
-          return false;
+          console.warn(this.formData, valid, rules);
         }
       });
     },
     handleReset() {
-      this.$refs.ruleForm.resetFields();
+      this.$refs.form.resetFields();
     },
   },
 };
