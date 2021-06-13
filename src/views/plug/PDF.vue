7147daceb852ef3c55620b5ce3e264543d18baa5
gitwd1998 <wd15855385074@outlook.com>
2021-06-13 18:19:20 +0800

    ADD vue中pdf文件预览（未实现）

---
+++ src/views/plug/PDF.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-dialog :visible.sync="showPDF">
+    <!-- <pdf src="../../assets/pdf/简历.pdf" :page="1"></pdf> -->
+  </el-dialog>
+</template>
+<script>
+import { Dialog } from "element-ui";
+// import pdf from "vue-pdf";
+export default {
+  components: {
+    elDialog: Dialog,
+    // pdf,
+  },
+  data() {
+    return {
+      showPDF: true,
+    };
+  },
+};
+</script>
