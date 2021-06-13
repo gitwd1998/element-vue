02bd8c404b359160b32b884505f78453e40bf9bf
gitwd1998 <wd15855385074@outlook.com>
2021-06-13 18:12:59 +0800

    MOD 修改获取国际化的方式以及存储方式

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import store from '../store'
 import en from './lang/en'
 import zh from './lang/zh'
 import locale from 'element-ui/lib/locale'
 Vue.use(VueI18n)
 const i18n = new VueI18n({
-    locale: sessionStorage.getItem("lang") || 'zh',
+    locale: store.getters.getLang,
     messages: { en, zh, }
 })
 locale.i18n((key, value) => i18n.t(key, value))//为了实现element-ui的多语言切换
