af9f40f4eedf0ac66dfabf5e8e05a7c798da64c6
gitwd1998 <wd15855385074@outlook.com>
2021-06-13 18:14:01 +0800

    ADD 新增路由导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Info from '@/views/Info'
+import store from '../store'
+// const Info = r => require.ensure([], () => r(require('@/views/Info.vue')), 'Info')// 同上
 
 Vue.use(VueRouter)
 
 const routes = [
   // info
-  { path: '/', component: Info },
+  { path: '/', name: 'info', component: Info },
   // basic
-  { path: '/button', component: () => import('@/views/basic/Button') },
-  { path: '/layout', component: () => import('@/views/basic/Layout') },
-  { path: '/color', component: () => import('@/views/basic/Color') },
+  { path: '/button', name: 'button', component: () => import('@/views/basic/Button') },
+  { path: '/layout', name: 'layout', component: () => import('@/views/basic/Layout') },
+  { path: '/color', name: 'color', component: () => import('@/views/basic/Color') },
   // data
-  { path: '/table', component: () => import('@/views/data/Table') },
+  { path: '/table', name: 'table', component: () => import('@/views/data/Table') },
   // form
-  { path: '/timePicker', component: () => import('@/views/form/TimePicker') },
-  { path: '/datePicker', component: () => import('@/views/form/DatePicker') },
-  { path: '/transfer', component: () => import('@/views/form/Transfer') },
-  { path: '/upload', component: () => import('@/views/form/Upload') },
-  { path: '/form', component: () => import('@/views/form/Form') },
+  { path: '/timePicker', name: 'timePicker', component: () => import('@/views/form/TimePicker') },
+  { path: '/datePicker', name: 'datePicker', component: () => import('@/views/form/DatePicker') },
+  { path: '/transfer', name: 'transfer', component: () => import('@/views/form/Transfer') },
+  { path: '/upload', name: 'upload', component: () => import('@/views/form/Upload') },
+  { path: '/form', name: 'form', component: () => import('@/views/form/Form') },
   // navigation
-  { path: '/navMenu', component: () => import('@/views/navigation/NavMenu') },
+  { path: '/navMenu', name: 'navMenu', component: () => import('@/views/navigation/NavMenu') },
   // notice
   // other
-  { path: '/calendar', component: () => import('@/views/others/Calendar') },
+  { path: '/calendar', name: 'calendar', component: () => import('@/views/others/Calendar') },
   // plug
-  { path: '/editor', component: () => import('@/views/plug/Editor') },
-  { path: '/mavonEditor', component: () => import('@/views/plug/MavonEditor') },
-  { path: '/vxeTable', component: () => import('@/views/plug/VXETable') },
+  { path: '/wangEditor', name: 'wangEditor', component: () => import('@/views/plug/WangEditor') },
+  { path: '/mavonEditor', name: 'mavonEditor', component: () => import('@/views/plug/MavonEditor') },
+  { path: '/vxeTable', name: 'vxeTable', component: () => import('@/views/plug/VXETable') },
+  { path: '/pdf', name: 'pdf', component: () => import('@/views/plug/PDF') },
 
 ]
 
@@ -44,8 +47,24 @@ const router = new VueRouter({
   }
 })
 
+// 路由白名单
+const whiteList = ['/']
 
-// 导航守卫
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  // 判断是是否带有token
+  let token = store.getters.getToken
+  let { path } = to
+  if (whiteList.includes(path)) {
+    next()
+  } else {
+    if (token) {
+      next()
+    } else {
+      next('/')
+    }
+  }
+})
 
 
 
