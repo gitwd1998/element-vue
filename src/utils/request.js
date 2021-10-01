0545e750b01b345d1aed20ead38e48d3de9fe155
王冬 <wangdong@wangdongdeMacBook-Air.local>
2021-10-01 20:31:51 +0800

    MOD 调整

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,50 +3,37 @@ import router from "../router";
 import axios from 'axios'
 import { Message } from "element-ui";
 // axios.defaults.withCredentials = true;//配合后端拿到cookie中的值（打包后放到服务器上则不需要）
-// 将token信息加到请求头中
-// axios.defaults.headers.Authorization = localStorage.getItem('token')
 console.warn(process.env)
 
-let service = axios.create({
-    baseURL: process.env.VUE_APP_BAES_URL,
-    timeout: 3000,
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BAES_URL,
+  timeout: 3000,
 })
+
+// 接口白名单不需要校验token的api
+const apiwhitelist = ['/captcha', '/login', '/regist']
 
 // request interceptor(请求拦截器)
 service.interceptors.request.use((request) => {
-    // 前端请求头中的字段都会转成首字母, 但后端接收到的都是小写
-    request.headers['Authorization'] = localStorage.getItem('token')
-    let { url, headers } = request
-    // 白名单, 不需要进行验证token的api
-    var urlArr = ['/captcha', '/login', '/regist']
-    if (urlArr.includes(url)) return Promise.resolve(request)
-    const token = headers.Authorization
-    if (!token) Message.error("前端拦截: 没有获取到token")
-    // 在这做限制, 如果没有获取到token就不请求
-    return Promise.resolve(request)
+  // 前端请求头中的字段都会转成首字母, 但后端接收到的都是小写
+  request.headers['Authorization'] = store.getters.getToken || ''
+  const { url, headers } = request
+  if (apiwhitelist.includes(url)) return Promise.resolve(request)
+  const token = headers.Authorization
+  if (token) return Promise.resolve(request)
+  Message.error("前端拦截: 没有获取到token")
+  router.replace({ path: '/login', name: 'login', query: { type: 1 } })
 }, (error) => {
-    Message.error(`request-err: ${error}`)
-    return Promise.reject(error)
+  Message.error(`request-err: ${error}`)
+  return Promise.reject(error)
 })
 
 // respone interceptor(应答拦截器)
 service.interceptors.response.use((response) => {
-    let { data } = response
-    if (Number(data.code) === 401) Message.error(data.msg)
-    if (data.name === "TokenExpiredError") {
-        Message.error("token已过期! 请重新登录")
-        localStorage.clear();
-        store.commit("setUser", "");
-        store.commit("setPass", "");
-        store.commit("setToken", "");
-        store.commit("setPhone", "");
-        store.commit("setTimestamp", "");
-        router.push({ path: "/login", name: "login", query: { type: 1 } })
-    }
-    return Promise.resolve(response);
+  return Promise.resolve(response.data);
 }, (error) => {
-    Message.error(`response-err: ${error}`)
-    return Promise.reject(error)
+  Message.error(`response-err: ${error}`)
+  return Promise.reject(error)
 })
 
 export default service
