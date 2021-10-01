96ebfc48b99ed2cb7152087260ea756fe529a691
王冬 <wangdong@wangdongdeMacBook-Air.local>
2021-10-02 00:09:21 +0800

    MOD 添加滚动条

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,12 +2,16 @@
   <el-container class="app">
     <el-header><component-header /></el-header>
     <el-container>
-      <el-aside width="auto"><component-aside /></el-aside>
+      <el-aside width="auto">
+        <el-scrollbar><component-aside /></el-scrollbar>
+      </el-aside>
       <el-main>
-        <transition name="page"><router-view /></transition>
-        <el-backtop target=".app .el-container .el-main">
-          <i class="el-icon-arrow-up" />
-        </el-backtop>
+        <el-scrollbar>
+          <transition name="page"><router-view /></transition>
+          <el-backtop target=".app .el-main .el-scrollbar__wrap">
+            <i class="el-icon-arrow-up" />
+          </el-backtop>
+        </el-scrollbar>
       </el-main>
     </el-container>
     <el-footer>--- {{ $t("footer.footnote") }} ---</el-footer>
@@ -17,7 +21,15 @@
 import componentAside from "@/layout/Aside";
 import componentHeader from "@/layout/Header";
 import { getDomHeight } from "@/utils";
-import { Aside, Container, Footer, Header, Main, Backtop } from "element-ui";
+import {
+  Aside,
+  Container,
+  Footer,
+  Header,
+  Main,
+  Backtop,
+  Scrollbar,
+} from "element-ui";
 export default {
   components: {
     componentAside: componentAside,
@@ -28,11 +40,39 @@ export default {
     elMain: Main,
     elFooter: Footer,
     elBacktop: Backtop,
+    elScrollbar: Scrollbar,
   },
-  created() {
+  mounted() {
     this.$nextTick(() => {
       this.calcMainHeigth();
     });
+    // 监听dom树变化
+    // let MutationObserver =
+    //   window.MutationObserver ||
+    //   window.WebkitMutationObserver ||
+    //   window.MozMutationObserver;
+    // let observer = new MutationObserver((mutationsList, observer) => {
+    //   console.log(mutationsList, observer);
+    //   this.calcMainHeigth();
+    // });
+    // observer.observe(document.getElementsByClassName("app")[0], {
+    //   attributes: true,
+    //   children: true,
+    //   subtree: true,
+    // });
+    window.onresize = () => {
+      this.calcMainHeigth();
+    };
+    let aside = document.getElementsByClassName("el-aside")[0];
+    document.onclick = (e) => {
+      console.log(aside.contains(e.target));
+      console.log(aside.isEqualNode(e.target));
+      console.log(aside.isSameNode(e.target));
+    };
+  },
+  beforeDestroy() {
+    // 否则当获取不到.el-container元素的时候会报错
+    document.onresize = null;
   },
   methods: {
     calcMainHeigth() {
@@ -49,22 +89,31 @@ export default {
 <style lang="less">
 .app {
   height: 100%;
+  background: #ffffff;
   .el-main {
-    position: relative;
-    overflow-x: hidden;
-    .box {
-      position: absolute;
-      left: 20px;
-      right: 20px;
-      top: 20px;
+    padding: 20px 0;
+    .el-scrollbar__view {
+      position: relative;
+      .box {
+        position: absolute;
+        left: 20px;
+        right: 20px;
+      }
+    }
+  }
+  .el-scrollbar {
+    height: 100%;
+    .el-scrollbar__wrap {
+      overflow-x: hidden;
     }
   }
   .el-footer {
     text-align: center;
+    background: radial-gradient(#fff, #eee, #fff);
     font: lighter 12px/60px "youyuan";
   }
   .page-enter {
-    transform: translate(100px, -100px);
+    transform: translate(100%, 0);
     opacity: 0;
   }
   .page-enter-active {
@@ -80,7 +129,7 @@ export default {
     transition: all 0.5s;
   }
   .page-leave-to {
-    transform: translate(-100px, 100px);
+    transform: translate(-100%, 0);
     opacity: 0;
   }
 }
